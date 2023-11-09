e59e4c6602f3165e5b3e51fd263e18c3378001e2
Dan Chernov <120645560+ChernovDan42@users.noreply.github.com>
2023-11-09 19:23:36 +0100

    up

--- src/components/Contacts/ContactsFilter/ContactsFilter.jsx
+++ src/components/Contacts/ContactsFilter/ContactsFilter.jsx
@@ -1,6 +1,7 @@
 import { useDispatch } from 'react-redux';
 import { filterChange } from 'redux/filterSlice';
-import { FormControl, FormLabel, Input } from '@chakra-ui/react';
+import { FormControl, FormLabel, Input, Icon, Box } from '@chakra-ui/react';
+import { RiUserSearchLine } from 'react-icons/ri';
 
 export const ContactsFilter = () => {
   const dispatch = useDispatch();
@@ -12,14 +13,26 @@ export const ContactsFilter = () => {
 
   return (
     <FormControl display="flex" alignItems="baseline" mt={5}>
-      <FormLabel fontSize={20}>Find contact</FormLabel>
-      <Input
-        type="text"
-        autoComplete="off"
-        onChange={handleChange}
-        w={377}
-        _focus={{ border: '1px solid #d5a6bd' }}
-      />
+      <FormLabel htmlFor="searchContact" fontSize={20}>
+        Find contact
+      </FormLabel>
+      <Box position="relative">
+        <Input
+          id="searchContact"
+          type="text"
+          autoComplete="off"
+          onChange={handleChange}
+          w={377}
+          _focus={{ border: '1px solid #d5a6bd' }}
+        />
+        <Icon
+          as={RiUserSearchLine}
+          position="absolute"
+          right={1}
+          top={'20%'}
+          boxSize="24px"
+        />
+      </Box>
     </FormControl>
   );
 };
