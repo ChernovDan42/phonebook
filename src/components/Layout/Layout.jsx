1d137abf5c20da17fc38be761dd6332343ec0f48
Dan Chernov <120645560+ChernovDan42@users.noreply.github.com>
2023-11-23 20:08:47 +0100

    Update Layout.jsx

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -2,7 +2,6 @@ import { Outlet } from 'react-router-dom';
 import { AppBar } from '../AppBar/AppBar';
 import { Suspense } from 'react';
 import { Loader } from 'components/helpers/Loader/Loader';
-import { Box } from '@chakra-ui/react';
 
 export const Layout = () => {
   return (
